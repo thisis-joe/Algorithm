521fad4e3bafc62b1a4e3f0527011977b2c6cabf
thisis-joe <96711842+thisis-joe@users.noreply.github.com>
2023-03-17 19:45:39 +0900

    [Silver I] Title: 회의실 배정, Time: 32 ms, Memory: 3688 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.cc
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.cc
@@ -1,34 +1,31 @@
-
 #include <bits/stdc++.h>
 using namespace std;
-#define X first 
-#define Y second 
 
-int n;
-pair<int, int> s[100005];
-int ans,t;
+
+vector<pair<int, int>> schedule;
 
 int main(void){
     ios::sync_with_stdio(0);
     cin.tie(0);
+    int n,end,begin;
 
     cin>>n;
     for(int i=0;i<n;i++){
-        cin>>s[i].Y>>s[i].X;
+        cin>>begin>>end;
+        schedule.push_back(make_pair(end,begin)); //종료지점에 대해 정렬하기 위해..
     }
-
-    sort(s,s+n); //first기준 정렬됨
     
-    for(int i=0;i<n;i++){
-        if(t>s[i].Y) continue;
-        else{
+    sort(schedule.begin(),schedule.end()); //end에 대해 정렬  (종료지점에 대해 정렬)
+    
+    int time = schedule[0].first;  //가장 빠른 종료시점
+    int ans = 1; // 최초 1개 무조건 등록
+    for(int i=1;i<n;i++){
+        if(time <= schedule[i].second) {
             ans++;
-            t=s[i].X;
+            time=schedule[i].first;
         }
     }
     
     cout<<ans;
-
-
     return 0;
 }
