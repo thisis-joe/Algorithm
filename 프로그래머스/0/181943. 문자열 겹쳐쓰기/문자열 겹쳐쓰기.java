2418a242b6e6587102792fb1c03a4ab52bc2e3f3
우동균 <96711842+thisis-joe@users.noreply.github.com>
2024-07-09 03:54:33 -0400

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.09 ms, Memory: 71.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Solution {
+    public String solution(String my_string, String overwrite_string, int s) {
+        String answer = "";
+        StringBuilder sb = new StringBuilder(my_string);
+        
+        for (int i = 0; i < overwrite_string.length(); i++) {
+            sb.setCharAt(s + i, overwrite_string.charAt(i));
+        }
+        
+        return sb.toString();
+        
+    }
+}
