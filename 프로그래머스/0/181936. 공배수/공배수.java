cd631370473ff53ca990bf29714e002172830c68
우동균 <96711842+thisis-joe@users.noreply.github.com>
2024-07-10 02:58:47 -0400

    [level 0] Title: 공배수, Time: 0.02 ms, Memory: 70.6 MB -BaekjoonHub

--- 프로그래머스/0/181936. 공배수/공배수.java
+++ 프로그래머스/0/181936. 공배수/공배수.java
@@ -1,14 +1,6 @@
-import java.util.*;
+
 class Solution {
     public int solution(int number, int n, int m) {
-        if(number % lcm(n,m)==0) return 1;
-        else return 0;
-    }
-    private int gcd(int a, int b){
-        if (b==0) return a;
-        return gcd(b, a%b);
-    }
-    private int lcm(int a, int b){
-        return a * (b/gcd(a,b));
-    }
+        return number % n == 0 && number % m == 0 ? 1 : 0;
+}
 }
