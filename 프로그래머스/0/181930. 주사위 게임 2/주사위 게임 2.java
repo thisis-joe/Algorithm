22841307bfb4692f8cf85ea00439af12bd14d817
우동균 <96711842+thisis-joe@users.noreply.github.com>
2024-07-11 03:02:08 -0400

    [level 0] Title: 주사위 게임 2, Time: 0.02 ms, Memory: 76.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int a, int b, int c) {
+        if(a!=b && b!=c && a!=c) 
+            return a+b+c;
+        else if((a==b && b!=c)||(a==c && a!=b)||(b==c && a!=b)) 
+            return (a + b + c) * (a*a + b*b + c*c);
+        else 
+            return (a + b + c) * (a*a + b*b + c*c) * (a*a*a + b*b*b + c*c*c);
+    }
+}
