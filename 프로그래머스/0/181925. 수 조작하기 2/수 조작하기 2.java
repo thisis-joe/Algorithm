25cfc42f71574d6419aec3f13bdd826a9fd0361d
우동균 <96711842+thisis-joe@users.noreply.github.com>
2024-07-11 03:49:16 -0400

    [level 0] Title: 수 조작하기 2, Time: 6.86 ms, Memory: 89.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public String solution(int[] numLog) {
+        StringBuilder answer = new StringBuilder();
+        
+        for(int i=0 ; i<numLog.length-1 ; i++){
+            
+            int move = numLog[i+1]-numLog[i];
+            switch(move){
+                case 1:
+                    answer.append("w"); break;
+                case -1:
+                    answer.append("s"); break;
+                case 10:
+                    answer.append("d"); break;
+                case -10:
+                    answer.append("a"); break;       
+            }
+        }
+        
+        return answer.toString();
+    }
+}
