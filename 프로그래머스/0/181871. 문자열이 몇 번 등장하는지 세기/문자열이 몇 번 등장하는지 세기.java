d2b465a8a66c0bceec766e837810bb5d37285eee
우동균 <96711842+thisis-joe@users.noreply.github.com>
2024-09-04 23:18:14 -0400

    [level 0] Title: 문자열이 몇 번 등장하는지 세기, Time: 0.08 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/0/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.java
+++ 프로그래머스/0/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.java
@@ -1,9 +1,13 @@
 class Solution {
     public int solution(String myString, String pat) {
-        int count=0;
-        for(int i=0;i<=myString.length()-pat.length();i++){
-            if((myString.substring(i,i+pat.length())).equals(pat)) count++;
+        int cnt = 0;
+        // 루프 조건을 <= myString.length() - pat.length()로 변경하여 불필요한 호출 감소
+        for (int i = 0; i <= myString.length() - pat.length(); i++) {
+            // startsWith를 사용하여 i번째 위치에서 시작하는지 확인
+            if (myString.startsWith(pat, i)) {
+                cnt++;
+            }
         }
-        return count;
+        return cnt;
     }
 }
