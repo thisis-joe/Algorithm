74a33a17f3a7e06ade1f0c23eb1a5ccbee2b38a2
우동균 <96711842+thisis-joe@users.noreply.github.com>
2024-09-16 15:07:56 +0900

    [level 0] Title: 뒤에서 5등까지, Time: 0.54 ms, Memory: 70.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] num_list) {
+        Arrays.sort(num_list);
+        int[] answer = {num_list[0],num_list[1],num_list[2],num_list[3],num_list[4]};
+        return answer;
+    }
+}
