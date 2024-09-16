1b3c329e6b72e66834a313e5466380613316b216
우동균 <96711842+thisis-joe@users.noreply.github.com>
2024-09-16 14:35:32 +0900

    [level 0] Title: 문자열 묶기, Time: 10.07 ms, Memory: 108 MB -BaekjoonHub

--- 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.java
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.java
@@ -2,7 +2,7 @@ import java.util.*;
 class Solution {
     public int solution(String[] strArr) {
         // 최대 길이를 알 수 있으므로 count 배열을 사용하여 각 길이에 대한 빈도를 기록
-        int[] count = new int[100001];
+        int[] count = new int[100000];
 
         // 문자열 길이에 따라 count 배열에 빈도 기록
         for (String str : strArr) {
