8404ef3571e7706868709364ca7902b51fe952f0
우동균 <96711842+thisis-joe@users.noreply.github.com>
2024-09-24 15:48:00 +0900

    [level 0] Title: 2의 영역, Time: 2.55 ms, Memory: 104 MB -BaekjoonHub

--- 프로그래머스/0/181894. 2의 영역/2의 영역.java
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.java
@@ -2,36 +2,18 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] arr) { 
-       int firstIndex = -1;
-        int lastIndex = -1;
+        int firstIdx=-1;
+        int lastIdx=-1;
         
-        // Find the first occurrence of 2
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == 2) {
-                firstIndex = i;
-                break;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==2){
+                if(firstIdx==-1)
+                    firstIdx=i;
+                lastIdx=i;
             }
         }
         
-        // If there is no 2 in the array, return [-1]
-        if (firstIndex == -1) {
-            return new int[] {-1};
-        }
-        
-        // Find the last occurrence of 2
-        for (int i = arr.length - 1; i >= 0; i--) {
-            if (arr[i] == 2) {
-                lastIndex = i;
-                break;
-            }
-        }
-        
-        // Create the result array with the subarray from firstIndex to lastIndex
-        int[] result = new int[lastIndex - firstIndex + 1];
-        for (int i = firstIndex; i <= lastIndex; i++) {
-            result[i - firstIndex] = arr[i];
-        }
-        
-        return result;
+        if(firstIdx==-1) return new int[]{-1};
+        return Arrays.copyOfRange(arr,firstIdx,lastIdx+1); //+1해주자
     }
 }
