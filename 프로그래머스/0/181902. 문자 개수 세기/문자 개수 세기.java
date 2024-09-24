8ff06cdd87653ab1ffdf12a03a2b8bdd018b4d21
우동균 <96711842+thisis-joe@users.noreply.github.com>
2024-09-24 15:24:10 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.19 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.java
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.java
@@ -4,11 +4,13 @@
 class Solution {
     public int[] solution(String my_string) {
         int[] answer = new int[52];
-        
         for(int i=0;i<my_string.length();i++){
-            char c = my_string.charAt(i);
-            if(c>='A'&&c<='Z') answer[c-'A']++;
-            else if(c>='a'&&c<='z') answer[c-'a'+26]++;
+            if(my_string.charAt(i)>='A' && my_string.charAt(i)<='Z'){
+                answer[my_string.charAt(i)-'A']++;
+            }
+            else if(my_string.charAt(i)>='a' && my_string.charAt(i)<='z'){
+                answer[my_string.charAt(i)-'a'+26]++;
+            }
         }
         return answer;
     }
