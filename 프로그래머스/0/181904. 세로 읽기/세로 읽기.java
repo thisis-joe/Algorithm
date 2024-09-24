244625a1c1500c8ab3e2a370df5212f05504b984
우동균 <96711842+thisis-joe@users.noreply.github.com>
2024-09-24 15:00:31 +0900

    [level 0] Title: 세로 읽기, Time: 0.32 ms, Memory: 79.3 MB -BaekjoonHub

--- 프로그래머스/0/181904. 세로 읽기/세로 읽기.java
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.java
@@ -2,14 +2,31 @@ import java.util.*;
 
 class Solution {
     public String solution(String my_string, int m, int c) {
+        StringBuilder sb = new StringBuilder();
+        int len = my_string.length();
+        int numSegments = (len % m == 0) ? len / m : len / m + 1;
+        String[] strArr = new String[numSegments]; // 정확한 세그먼트 개수 계산
 
-        StringBuilder result = new StringBuilder();
-
-        // 문자열 my_string을 m 길이로 잘라서 세로로 c번째 열에 해당하는 문자들을 추출
-        for (int i = c - 1; i < my_string.length(); i += m) {
-            result.append(my_string.charAt(i));
+        int s = 0;
+        int e = m;
+        
+        // 세그먼트 생성
+        for (int i = 0; i < numSegments; i++) {
+            if (e <= len) {
+                strArr[i] = my_string.substring(s, e);
+            } else {
+                strArr[i] = my_string.substring(s); // 마지막 세그먼트는 남은 문자열 전체
+            }
+            s += m;
+            e += m;
         }
 
-        return result.toString();
+        // 각 세그먼트에서 c번째 문자 추출
+        for (String str : strArr) {
+            if (str.length() >= c) {
+                sb.append(str.charAt(c - 1)); // c번째 문자는 인덱스 c-1
+            }
+        }
+        return sb.toString();
     }
 }
