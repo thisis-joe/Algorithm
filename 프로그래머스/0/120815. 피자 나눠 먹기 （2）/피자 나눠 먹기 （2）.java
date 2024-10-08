fa95abffabe4ea13fcfd78509f67e0349a8aedad
우동균 <96711842+thisis-joe@users.noreply.github.com>
2024-10-08 01:48:35 -0400

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.03 ms, Memory: 68.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int n) {
+        int k=1;
+        
+            while(6*k%n!=0){
+                k++;
+            }
+        
+        return k;
+    }
+}
