286bc903ca73edecc36da666608211dfbc9e2ec1
우동균 <96711842+thisis-joe@users.noreply.github.com>
2024-10-16 01:42:21 -0400

    [level 0] Title: 삼각형의 완성조건 (2), Time: 0.04 ms, Memory: 86.8 MB -BaekjoonHub

--- 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.java
+++ 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.java
@@ -1,20 +1,7 @@
-import java.util.*;
 class Solution {
     public int solution(int[] sides) {
-        int side1 = Math.max(sides[0],sides[1]);
-        int side2 = Math.min(sides[0],sides[1]);
-        int side3 = side1;
-        int cnt = 0;
-        
-        //side1 이 가장 길 때
-        for(int i=1; i<side1; i++){
-            if(i+side2>side1) cnt++;
-        }
-        //side3 가 가장 길 때
-        while(side1+side2>side3){
-            cnt++;
-            side3++;
-        }
-        return cnt;
+        return Math.min(sides[0], sides[1]) * 2 - 1;
+
+         
     }
 }
