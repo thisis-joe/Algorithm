86ceed4471574a1488f6adf25ccb599e8ae5daa7
우동균 <96711842+thisis-joe@users.noreply.github.com>
2024-10-22 01:40:46 -0400

    [level 0] Title: 유한소수 판별하기, Time: 0.03 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.java
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.java
@@ -1,42 +1,6 @@
 class Solution {
     public int solution(int a, int b) {
-        //기약분수 만들기
-        int numerator = a; 
-        int denominator = b;
-        for(int i=2;i<=Math.min(a,b);i++){
-            if(numerator%i==0 && denominator%i==0) {
-                numerator /= i;
-                denominator /= i;
-                i--;
-            }
-        }
-                //System.out.println(numerator+" , "+denominator);
-        //분모 소인수 분해 및 유한소수 판별
-        for(int i=2;i<=denominator;i++){
-            int flag=0;
-            if(isSosu(i)==1){
-                if(denominator%i==0){
-                    if(i!=2 && i!=5) return 2;
-                }  
-            }
-            else continue;
-        }
-        return 1;
+        int answer = ((a*320000)%b == 0) ? 1 : 2;
+        return answer;
     }
-    
-    //소수판별
-    public int isSosu(int num){
-        if(num==2){
-            return 1;
-        }
-        else{
-            for(int i=2;i<num;i++){
-                if(num%i==0){
-                    return 2;
-                }
-            }
-        }
-        return 1;
-    }
-    
 }
