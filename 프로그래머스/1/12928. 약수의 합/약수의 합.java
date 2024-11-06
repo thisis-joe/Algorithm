a55d3d1c19162aecc9f8a675123dd649aa82ed3a
우동균 <96711842+thisis-joe@users.noreply.github.com>
2024-11-06 06:20:41 -0500

    [level 1] Title: 약수의 합, Time: 0.06 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.java
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.java
@@ -1,11 +1,14 @@
 import java.util.*;
 
 class Solution {
-    public int solution(int n) {
+    public int sumDivisor(int n){
         int sum=0;
-        for(int i=1;i<=n;i++){
-            if(n%i==0) sum+=i;
+        for(int i=1;i<=n/2;i++){
+            if(n%i==0)sum+=i;
         }
-        return sum;
+        return sum+n;
+    }
+    public int solution(int n) {
+        return sumDivisor(n);
     }
 }
