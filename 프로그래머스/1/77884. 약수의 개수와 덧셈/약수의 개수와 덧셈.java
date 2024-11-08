321ccd367a4c8a8d2a0b02303e4e913c0f8f281d
우동균 <96711842+thisis-joe@users.noreply.github.com>
2024-11-08 01:38:25 -0500

    [level 1] Title: 약수의 개수와 덧셈, Time: 2.16 ms, Memory: 71.3 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
@@ -4,14 +4,15 @@ class Solution {
         for(int i=1;i<=n/2;i++){
             if(n%i==0) cnt++;
         }
-        return cnt;
+        return cnt+1; //자기자신 1개 더해야함
     }
+    
     public int solution(int left, int right) {
         int sum=0;
         
         for(int i=left;i<=right;i++){
-            if(YAKSUcount(i)%2==0) sum-=i;
-            else sum+=i;
+            if(YAKSUcount(i)%2==0) sum+=i;
+            else sum-=i;
         }
         
         return sum;
