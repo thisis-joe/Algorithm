135faa7ec997dcf83832d52878eca6d591b4cadf
우동균 <96711842+thisis-joe@users.noreply.github.com>
2024-11-19 01:33:30 -0500

    [level 1] Title: [1차] 비밀지도, Time: 16.58 ms, Memory: 87.6 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,26 +1,20 @@
 import java.util.*;
-
 class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
-        String[] resMap = new String[n];
-
-        for (int i = 0; i < n; i++) {
-            // 이진수 변환 및 길이 n 맞추기
-            String binary1 = String.format("%" + n + "s", Integer.toBinaryString(arr1[i])).replace(' ', '0');
-            String binary2 = String.format("%" + n + "s", Integer.toBinaryString(arr2[i])).replace(' ', '0');
+        String[] resmap = new String[n];
+        Arrays.fill(resmap,"");
+        
+        for(int i=0;i<n;i++){
+            String row4map1 = String.format("%"+n+"s",Integer.toBinaryString(arr1[i])).replace(' ','0');   
+            String row4map2 = String.format("%"+n+"s",Integer.toBinaryString(arr2[i])).replace(' ','0');   
             
-            // 두 지도의 한 행 합치기
-            StringBuilder row = new StringBuilder();
-            for (int j = 0; j < n; j++) {
-                if (binary1.charAt(j) == '1' || binary2.charAt(j) == '1') {
-                    row.append("#");
-                } else {
-                    row.append(" ");
-                }
+            for(int j=0;j<n;j++){ //row4map1의 길이까지 이지만 어차피 그게 n이다
+                if(row4map1.charAt(j)=='0'&& row4map2.charAt(j)=='0') resmap[i]+=" ";
+                else resmap[i]+="#";
             }
-            resMap[i] = row.toString();
         }
-
-        return resMap;
+        
+        return resmap;
+        
     }
 }
