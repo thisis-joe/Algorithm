767699da73995184b1ead30e8769555c1bd341fd
우동균 <96711842+thisis-joe@users.noreply.github.com>
2024-12-13 14:03:08 +0900

    [level 1] Title: 달리기 경주, Time: 339.85 ms, Memory: 374 MB -BaekjoonHub

---
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class Solution {
+    public String[] solution(String[] players, String[] callings) {
+        Map<String, Integer> rank = new HashMap<>();
+        
+        for(int i=0;i<players.length;i++){
+            rank.put(players[i],i);
+        }
+        
+        for(String call : callings){
+            //순위 파악
+            int callRank = rank.get(call);
+            int preRank  = rank.get(call)-1;
+            
+            // HashMap 업데이트
+            rank.put(call, preRank);
+            rank.put(players[preRank], callRank);
+            
+            //순위 변경
+            String temp       = players[callRank];
+            players[callRank] = players[preRank];
+            players[preRank]  = temp;
+            
+        }
+        
+        
+        return players;
+        
+    }
+}
