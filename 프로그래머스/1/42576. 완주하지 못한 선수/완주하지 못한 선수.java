3cd1ddbb004c7f648cc9e0fb2128aeb7c2abe61a
우동균 <96711842+thisis-joe@users.noreply.github.com>
2025-03-13 10:36:30 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 73.30 ms, Memory: 97.4 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,36 +1,31 @@
 import java.util.*;
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        Map<String, Integer> map = new HashMap<>();
-        
-        // 키-값 이 이름-동명이인수 로 구성된 맵 작성
-        for(String person : participant){
-            if(map.get(person)!=null){ // 동명이인 시 1명 추가
-                map.put(person,map.get(person)+1); 
+        //마라톤 완주자 맵
+        Map<String, Integer> compMap = new HashMap<>();
+        //완주자 목록을 순회하면서 맵에 완주자 키-값이 없으면 (완주자 이름, +1) 등록
+        for(String str : completion){
+            if(compMap.get(str)==null){
+                compMap.put(str,1);    
             }
             else{
-                map.put(person,1); // person이 존재하지 않으면 1명으로 설정
+                compMap.put(str,compMap.get(str)+1);    
             }
         }
-        
-        for(String person : completion){
-            if(map.get(person)!=0){ // 0명이 아니면 1명을 낮춤.
-                map.put(person,map.get(person)-1);
+        //참가자 목록을 순회하면서, 완주자 맵에 존재하면 값-1수행, 존재하지 않으면 즉시 해당 이름을 반환
+        for(String str : participant){
+            if(compMap.get(str)==null){
+                return str;
+            }
+            else{
+                compMap.put(str,compMap.get(str)-1);
             }
         }
-        
-        //map에서 값이 0이 아닌 person을 반환
-        List<String> strList = new ArrayList<>();
-        Iterator<String> mapItr = map.keySet().iterator(); //iterator도 타입이 필요하며, map의 keySet에 대한 iterator로 지정.
-        
-        while(mapItr.hasNext()){
-            String key = mapItr.next(); // iterator에서 다음번을 가리키도록 함.
-            if(map.get(key)!=0){
-                strList.add(key);
-            }
+        //완주자 맵에서 값이 0이 아닌 사람 = 완주하지 못한 사람 (동명이인)
+        for(String key : compMap.keySet()){
+            if(compMap.get(key)!=0) return key;
         }
-        
-        return strList.get(0);
-        
+        //도달하지 않는 문장
+        return "";
     }
 }
