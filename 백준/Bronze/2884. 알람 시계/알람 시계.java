b87dd181300fb0dba5456488752b2f861464e6d5
우동균 <96711842+thisis-joe@users.noreply.github.com>
2025-05-21 11:03:53 +0900

    [Bronze III] Title: 알람 시계, Time: 188 ms, Memory: 18468 KB -BaekjoonHub

--- 백준/Bronze/2884. 알람 시계/알람 시계.java
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -1,19 +1,23 @@
-import java.util.Scanner;
+import java.util.*;
 
-public class Main {
-	public static void main(String[] args) {
-		Scanner scanner=new Scanner(System.in);
-		int H=scanner.nextInt();
-		int M=scanner.nextInt();
-		
-		M-=45; //m=10 이면 -35
-		if(M<0) {
-			M=60+M; //60-35=25
-			H-=1;
-		}
-		if(H<0) { //h=-1이면  밑에서 23 나와야됨
-			H=24+H;
-		}
-		System.out.println(H+" "+M);
-	}
+public class Main{
+    public static void main(String[] args){
+    	
+        Scanner sc = new Scanner(System.in);
+        int H = sc.nextInt();
+        int M = sc.nextInt();
+        
+        int a = M-45;
+        int b = 15+M;
+        
+        if(M >= 45) {
+        	System.out.println(H+" "+a);
+        }
+        else if(H==0) {
+        	System.out.println("23 "+b);
+        }
+        else {
+        	System.out.println(H-1+" "+b);
+        }
+    }
 }
