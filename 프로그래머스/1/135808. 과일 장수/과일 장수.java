459e299fed21fc57decbe2874ed83c2c9f4e34ea
우동균 <96711842+thisis-joe@users.noreply.github.com>
2025-06-04 08:36:47 +0900

    [level 1] Title: 과일 장수, Time: 84.72 ms, Memory: 130 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -1,13 +1,18 @@
 import java.util.*;
 class Solution {
     public int solution(int k, int m, int[] score) {
-        int benefit=0;
-        Integer[] score_new = Arrays.stream(score).boxed().toArray(Integer[]::new);
-        Arrays.sort(score_new,Collections.reverseOrder()); //내림차순 정렬
+        int answer = 0;
         
-        for(int i=m-1;i<score.length-score.length%m;i+=m){ //마지막 1개만 본다.
-            benefit += score_new[i] * m ; 
+        Arrays.sort(score);
+        for(int i = score.length-1; i >= 0 ; i-=m ){
+            if(i-m+1 < 0){
+                break;
+            }
+            else{
+                answer += score[i-m+1] * m ;    
+            }
         }
-        return benefit;
+        
+        return answer;
     }
 }
