76ece57db9dcd37bf476136fb60757de8e030f7b
우동균 <96711842+thisis-joe@users.noreply.github.com>
2025-06-17 12:18:17 +0900

    [level 1] Title: 둘만의 암호, Time: 2.11 ms, Memory: 83.5 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
@@ -1,32 +1,25 @@
-import java.util.*;
-
-class Solution {
+ class Solution {
     public String solution(String s, String skip, int index) {
-        char[] res = s.toCharArray();
-        String result = "";
-        Set<Character> skips = new HashSet<>();
-        
-        // Skip 문자 Set으로 저장 (검색 속도 향상)
-        for (char c : skip.toCharArray()) {
-            skips.add(c);
-        }
-        
-        // 각 문자 변환
-        for (int i = 0; i < res.length; i++) {
-            int steps = 0;
-            char current = res[i];
-            
-            // Index만큼 이동 (skip 문자 제외)
-            while (steps < index) {
-                current++;
-                if (current > 'z') current = 'a'; // Wrap-around 처리
-                if (!skips.contains(current)) steps++;
+        String answer = "";
+        //skip을 알파벳에서 없애고 시작하기.
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+        String[] delete = skip.split("");
+
+        for(String d:delete)
+            alphabet = alphabet.replace(d,"");
+
+        String[] myalphabet = alphabet.split("");
+        String[] sArr = s.split("");
+
+        for(int i = 0; i< sArr.length; i++){
+            for(int j = 0; j < myalphabet.length; j++){
+                if(sArr[i].equals(myalphabet[j])){
+                    answer+=myalphabet[(j+index)%myalphabet.length];
+                    continue;
+                }
             }
-            
-            res[i] = current; // 최종 변환된 문자 저장
         }
-        
-        // 배열을 문자열로 변환
-        return new String(res);
+
+        return answer;
     }
-}
+ }
