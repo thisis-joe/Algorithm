75272b1a2aa821dcb354d898854c270e70b7ef3c
우동균 <96711842+thisis-joe@users.noreply.github.com>
2025-06-17 12:02:28 +0900

    [level 1] Title: 둘만의 암호, Time: 13.59 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
@@ -1,32 +1,16 @@
-import java.util.*;
-
 class Solution {
     public String solution(String s, String skip, int index) {
-        char[] res = s.toCharArray();
-        String result = "";
-        Set<Character> skips = new HashSet<>();
+        char[] given = s.toCharArray();
         
-        // Skip 문자 Set으로 저장 (검색 속도 향상)
-        for (char c : skip.toCharArray()) {
-            skips.add(c);
-        }
-        
-        // 각 문자 변환
-        for (int i = 0; i < res.length; i++) {
-            int steps = 0;
-            char current = res[i];
-            
-            // Index만큼 이동 (skip 문자 제외)
-            while (steps < index) {
-                current++;
-                if (current > 'z') current = 'a'; // Wrap-around 처리
-                if (!skips.contains(current)) steps++;
+        for(int i = 0 ; i < given.length ; i++){
+            int move = 0;
+            while(move < index){
+                given[i]++;
+                if(given[i] > 'z') given[i] -= 26;
+                if(!skip.contains(given[i]+"")) move++;
             }
-            
-            res[i] = current; // 최종 변환된 문자 저장
         }
         
-        // 배열을 문자열로 변환
-        return new String(res);
+        return new String(given);
     }
 }
