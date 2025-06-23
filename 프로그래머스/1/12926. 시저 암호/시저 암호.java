a9cf893dd915a01c61e753ecbad5d978268babe2
우동균 <96711842+thisis-joe@users.noreply.github.com>
2025-06-23 10:07:44 +0900

    [level 1] Title: 시저 암호, Time: 1.29 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -2,24 +2,10 @@ class Solution {
     public String solution(String s, int n) {
         char[] charArr = s.toCharArray();
         for(int i = 0 ; i < charArr.length ; i++){
-            //공백
-            if(charArr[i]==' '){
-                continue;
-            }
-            //대문자 범위 벗어나는 경우
-            else if(charArr[i] >= 'A' 
-               && charArr[i] <= 'Z'
-               && (char)((int)charArr[i] + n) > 'Z'){
-                charArr[i] = (char)((int)charArr[i]+n-26);
-            }
-            //소문자 범위 벗어나는 경우
-            else if(charArr[i] >= 'a' 
-               && charArr[i] <= 'z'
-               && (char)((int)charArr[i] + n) > 'z'){
-                charArr[i] = (char)((int)charArr[i]+n-26);
-            }   
-            else{
-                charArr[i] = (char)((int)charArr[i]+n);
+            if(charArr[i]!=' '){
+                charArr[i] = Character.isUpperCase(charArr[i]) 
+                            ? (char)('A' + (charArr[i] - 'A' + n) % 26) 
+                            : (char)('a' + (charArr[i] - 'a' + n) % 26);
             }
         }
         return new String(charArr);
