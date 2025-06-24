b7803c0aea833fcf8f7da9972c7a60c849295147
우동균 <96711842+thisis-joe@users.noreply.github.com>
2025-06-24 14:40:18 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 9.26 ms, Memory: 85.2 MB -BaekjoonHub

--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
@@ -1,42 +1,58 @@
-import java.util.*;
 class Solution {
     public String solution(int[] numbers, String hand) {
-        StringBuilder sb = new StringBuilder();
-        int curPosL=10;
-        int curPosR=12;
-        for(int num :numbers){
-            if(num==0) num=11;
-            if(num ==1 || num ==4 || num == 7){
-                sb.append("L");
-                curPosL=num;
+        String myhand = (hand.charAt(0)+"").toUpperCase();
+        int[][] pos = {{3,1},{0,0},{0,1},{0,2},{1,0},{1,1},{1,2},{2,0},{2,1},{2,2}}; //0~9 위치
+        String answer = "";
+        int[] LPos = new int[]{3,0}; //초기손가락위치 지정!!!
+        int[] RPos = new int[]{3,2};
+        
+        for(int num : numbers){
+            if(num == 1 || num == 4 || num == 7){
+                answer += "L";
+                LPos[0] = pos[num][0];
+                LPos[1] = pos[num][1];
             }
-            else if(num ==3 || num ==6 || num == 9){
-                sb.append("R");
-                curPosR=num;
+            else if(num == 3 || num == 6 || num == 9){
+                answer += "R";
+                RPos[0] = pos[num][0];
+                RPos[1] = pos[num][1]; //오타 !!
             }
-            else{
-                int lenL = Math.abs(curPosL-num)%3+Math.abs(curPosL-num)/3;
-                int lenR = Math.abs(curPosR-num)%3+Math.abs(curPosR-num)/3;
-                if(lenR<lenL) {
-                    sb.append("R");
-                    curPosR=num;
+            else{ //2 5 8 0
+                int LtoNum = getDistance(LPos,pos[num]);
+                int RtoNum = getDistance(RPos,pos[num]);
+                if(LtoNum < RtoNum){ //왼쪽 손가락과 더 가까움
+                    answer += "L";
+                    LPos[0] = pos[num][0];
+                    LPos[1] = pos[num][1];
                 }
-                else if(lenL<lenR) {
-                    sb.append("L");
-                    curPosL=num;
+                else if(LtoNum > RtoNum){ //오른쪽 손가락과 더 가까움
+                    answer += "R";
+                    RPos[0] = pos[num][0];
+                    RPos[1] = pos[num][1];
                 }
-                else {
-                    if(hand.equals("right")){
-                        sb.append("R");
-                        curPosR=num;
+                else{ //거리동일 
+                    answer += myhand;
+                    if(myhand.equals("L")){
+                        LPos[0] = pos[num][0];
+                        LPos[1] = pos[num][1];
                     }
                     else{
-                        sb.append("L");
-                        curPosL=num;
+                        RPos[0] = pos[num][0];
+                        RPos[1] = pos[num][1];
                     }
                 }
             }
+            
         }
-        return sb.toString();
+        return answer;
     }
+    
+    int getDistance(int[] pos1, int[] pos2){
+        return Math.abs(pos1[0] - pos2[0]) + Math.abs(pos1[1] - pos2[1]);
+    }
+    // double getDistance(int[] pos1, int[] pos2){
+    //     double x = Math.pow(Math.abs(pos1[0] - pos2[0]),2);
+    //     double y = Math.pow(Math.abs(pos1[1] - pos2[1]),2);
+    //     return Math.sqrt(x+y);
+    // }
 }
