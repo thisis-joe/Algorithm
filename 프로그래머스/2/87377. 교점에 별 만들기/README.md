ec712ebfa0be2d2340254e366cf3f64b2043a84d
우동균 <96711842+thisis-joe@users.noreply.github.com>
2025-06-24 18:50:27 +0900

    [level 2] Title: 교점에 별 만들기, Time: 26.13 ms, Memory: 95.3 MB -BaekjoonHub

--- 프로그래머스/2/87377. 교점에 별 만들기/README.md
+++ 프로그래머스/2/87377. 교점에 별 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 교점에 별 만들기 - 87377 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87377#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87377) 
 
 ### 성능 요약
 
-메모리: 80.6 MB, 시간: 20.41 ms
+메모리: 95.3 MB, 시간: 26.13 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 06일 19:20:16
+2025년 06월 24일 18:50:22
 
 ### 문제 설명
 
--- 프로그래머스/2/87377. 교점에 별 만들기/교점에 별 만들기.java
+++ 프로그래머스/2/87377. 교점에 별 만들기/교점에 별 만들기.java
@@ -1,82 +1,58 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
-public class Solution {
-    private static class Point {
-        public final long x, y;
-
-        private Point(long x, long y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
-
-    private Point intersection(long a1, long b1, long c1, long a2, long b2, long c2) {
-        double x = (double) (b1 * c2 - b2 * c1) / (a1 * b2 - a2 * b1);
-        double y = (double) (a2 * c1 - a1 * c2) / (a1 * b2 - a2 * b1);
-
-        if (x % 1 != 0 || y % 1 != 0) return null;
-
-        return new Point((long) x, (long) y);
-    }
-
-    private Point getMinimumPoint(List<Point> points) {
-        long x = Long.MAX_VALUE;
-        long y = Long.MAX_VALUE;
-
-        for (Point p : points) {
-            if (p.x < x) x = p.x;
-            if (p.y < y) y = p.y;
-        }
-
-        return new Point(x, y);
-    }
-
-    private Point getMaximumPoint(List<Point> points) {
-        long x = Long.MIN_VALUE;
-        long y = Long.MIN_VALUE;
-
-        for (Point p : points) {
-            if (p.x > x) x = p.x;
-            if (p.y > y) y = p.y;
-        }
-
-        return new Point(x, y);
-    }
-
+class Solution {
     public String[] solution(int[][] line) {
-        List<Point> points = new ArrayList<>();
-        for (int i = 0; i < line.length; i++) {
-            for (int j = i + 1; j < line.length; j++) {
-                Point intersection = intersection(line[i][0], line[i][1], line[i][2], line[j][0], line[j][1], line[j][2]);
-                if (intersection != null) {
-                    points.add(intersection);
-                }
+        
+        List<int[]> list = new ArrayList<>();
+        int minX = Integer.MAX_VALUE, maxX = Integer.MIN_VALUE;
+        int minY = Integer.MAX_VALUE, maxY = Integer.MIN_VALUE;
+        
+        //전체교점 구해서 저장
+        for(int i = 0 ; i < line.length-1 ; i++){
+            int A = line[i][0], B = line[i][1], E = line[i][2];
+            for(int j = i+1 ; j < line.length ; j++){
+                int C = line[j][0], D = line[j][1], F = line[j][2];
+                
+                long numerator1 = ((long)B*F - (long)E*D);
+                long numerator2 = ((long)E*C - (long)A*F);
+                long denomiator  = ((long)A*D - (long)B*C);
+                if( denomiator == 0 ) continue;
+                if( numerator1% denomiator != 0 || numerator2% denomiator != 0) continue;
+                
+                int newX = (int) (numerator1 / denomiator);
+                int newY = (int) (numerator2 / denomiator);
+
+                list.add(new int[]{newX,newY}); //중복이면 어쩌지
+
+                minX = Math.min(minX, newX);
+                maxX = Math.max(maxX, newX);
+                minY = Math.min(minY, newY);
+                maxY = Math.max(maxY, newY);
+                
+                
             }
         }
-
-        Point minimum = getMinimumPoint(points);
-        Point maximum = getMaximumPoint(points);
-
-        int width = (int) (maximum.x - minimum.x + 1);
-        int height = (int) (maximum.y - minimum.y + 1);
-
-        char[][] arr = new char[height][width];
-        for (char[] row : arr) {
-            Arrays.fill(row, '.');
+        
+        //그리기
+        int width = maxX - minX +1; //+1해야함
+        int height = maxY - minY +1;
+        char[][] map = new char[height][width];
+        
+        for (char[] row : map) Arrays.fill(row, '.');
+        
+        for (int[] point : list) {
+            int x = point[0];
+            int y = point[1];
+            int row = maxY - y;
+            int col = x - minX;
+            map[row][col] = '*';
+        }
+        
+        String[] answer = new String[height];
+        for (int i = 0; i < height; i++) {
+            answer[i] = new String(map[i]);
         }
 
-        for (Point p : points) {
-            int x = (int) (p.x - minimum.x);
-            int y = (int) (maximum.y - p.y);
-            arr[y][x] = '*';
-        }
-
-        String[] result = new String[arr.length];
-        for (int i = 0; i < result.length; i++) {
-            result[i] = new String(arr[i]);
-        }
-        return result;
+        return answer;
     }
 }
