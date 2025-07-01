0c7849327a6515d38a384c643ea9610d437f378a
우동균 <96711842+thisis-joe@users.noreply.github.com>
2025-07-01 12:57:20 +0900

    [level 2] Title: 주차 요금 계산, Time: 9.02 ms, Memory: 85.3 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
@@ -1,70 +1,31 @@
 import java.util.*;
-//10초
-class Solution {
-    public int[] solution(int[] fees, String[] records) {
-        // 차량번호, (입차시간, 출차시간) 의 자료구조 Map<Integer, Map<Integer,Integer>> myRecords = new HashMap<>();
-        // 차량별 누적 주차 시간을 계산하여 요금을 일괄로 정산합니다.
-        Map<Integer, Integer> inRecord = new HashMap<>();// 차량번호, 입차시간                   
-        Map<Integer, Integer> outRecord = new HashMap<>();// 차량번호, 출차시간
-        Map<Integer, Integer> totalTime = new HashMap<>();// 차량번호, 총 시간
-        
-        int baseTime = fees[0];
-        int baseFee = fees[1];
-        int unitTime = fees[2];
-        int unitFee = fees[3];
-        int maxTime = 23*60 + 59;
-        
-        for(String record : records){
-            String[] row = record.split(" ");
-            String[] timeStr = row[0].split(":");
-            int time = Integer.parseInt(timeStr[0]) * 60 + Integer.parseInt(timeStr[1]);
-            int car = Integer.parseInt(row[1]);
-            totalTime.putIfAbsent(car, 0); // 해당 자동차 번호등록, 시간은 0초기화 
-            
-            if(row[2].equals("IN")){
-                inRecord.put(car, time);
-                outRecord.put(car, maxTime); //기본적으로 출차시간은 23:59로 초기화
-                
-            }else if(row[2].equals("OUT")){
-                outRecord.put(car,time); //해당 차의 출차시각 덮어쓰기
-                    
-                int caseTime = outRecord.get(car) - inRecord.get(car);
-                totalTime.put(car,totalTime.get(car) + caseTime);
-                
-                inRecord.remove(car);
-                outRecord.remove(car); //Out을 만난경우 삭제
-            }
-        }
-        
-        //OUT 없는 IN 애들 시간 더해줌
-        for(Integer noOut : inRecord.keySet()){
-            int caseTime = maxTime - inRecord.get(noOut);
-            totalTime.put(noOut,totalTime.get(noOut) + caseTime);
-        }
-        
-        //정답 출력을 위해 배열로 변경
-        //차량 번호가 작은 자동차부터 - how??
-        //Map의 키 오름차순으로 정렬이 가능한가?
-        // 차량 번호 오름차순으로 정렬 후 요금 계산
-        
-        //총 시간에 대해 한번만 요금 계산
-        List<Integer> carList = new ArrayList<>(totalTime.keySet());
-        Collections.sort(carList);
-        int[] answer = new int[carList.size()];
-        
-        for (int i = 0; i < carList.size(); i++) {
-            int time = totalTime.get(carList.get(i));
-            if(time <= baseTime){
-                answer[i] = baseFee;   
-            }
-            else{
-                answer[i] = baseFee + (int)Math.ceil((double)(time-baseTime)/unitTime) * unitFee;
-            }
-            
-        }
 
-        return answer;
-        
-        
+class Solution {
+
+    public int timeToInt(String time) {
+        String temp[] = time.split(":");
+        return Integer.parseInt(temp[0])*60 + Integer.parseInt(temp[1]);
+    }
+    public int[] solution(int[] fees, String[] records) {
+
+        TreeMap<String, Integer> map = new TreeMap<>();
+
+        for(String record : records) {
+            String temp[] = record.split(" ");
+            int time = temp[2].equals("IN") ? -1 : 1;
+            time *= timeToInt(temp[0]);
+            map.put(temp[1], map.getOrDefault(temp[1], 0) + time);
+        }
+        int idx = 0, ans[] = new int[map.size()];
+        for(int time : map.values()) {
+            if(time < 1) time += 1439;
+            time -= fees[0];
+            int cost = fees[1];
+            if(time > 0)
+                cost += (time%fees[2] == 0 ? time/fees[2] : time/fees[2]+1)*fees[3];
+
+            ans[idx++] = cost;
+        }
+        return ans;
     }
 }
