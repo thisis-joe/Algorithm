afef1a87baf9d8a31bf647c8359c9beb731e4a03
우동균 <96711842+thisis-joe@users.noreply.github.com>
2025-07-11 21:28:44 +0900

    [level 2] Title: 구명보트, Time: 12.49 ms, Memory: 55.7 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -2,33 +2,24 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] people, int limit) {
-        /***
-        * 한번에 최대 2명
-        * 무게제한 40~limit, 최대 240
-        * 모든 사람 구출가능한 구명보트 최소 개수?
-        */
         
         int idxF = 0;
-        int idxB = people.length-1;
-        int left = people.length;
-        int count = 0;
-        
+        int idxB = people.length -1;
+        int boatCnt = 0;
         Arrays.sort(people);
-        
         while(idxF <= idxB){
-            if(people[idxF] + people[idxB] <= limit) {
-                count++;
-                left-=2;
-                idxB--;
+            if(people[idxF]+people[idxB]<=limit){
+                boatCnt++;
                 idxF++;
+                idxB--;
             }
             else{
-                count++;
-                left--;
+                boatCnt++;
                 idxB--;
             }
         }
         
-        return count;
+        return boatCnt;
+        
     }
 }
