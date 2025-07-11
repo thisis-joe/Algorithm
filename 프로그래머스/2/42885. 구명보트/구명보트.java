5807616712b11bd91e1b5ed4c31dace36847ae01
우동균 <96711842+thisis-joe@users.noreply.github.com>
2025-07-11 21:37:30 +0900

    [level 2] Title: 구명보트, Time: 12.36 ms, Memory: 58 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -4,22 +4,31 @@ class Solution {
     public int solution(int[] people, int limit) {
         
         int idxF = 0;
-        int idxB = people.length -1;
-        int boatCnt = 0;
+        int idxB = people.length-1;
+        int left = people.length;
+        int count = 0;
+        
         Arrays.sort(people);
-        while(idxF <= idxB){
-            if(people[idxF]+people[idxB]<=limit){
-                boatCnt++;
-                idxF++;
+        
+        while(left > 0){
+            if(idxB -1 < 0) {
+                count++;
+                break;
+            }
+            //2명경우 (1)
+            if(people[idxF] + people[idxB] <= limit) {
+                count++;
+                left-=2;
                 idxB--;
+                idxF++;
             }
             else{
-                boatCnt++;
+                count++;
+                left--;
                 idxB--;
             }
         }
         
-        return boatCnt;
-        
+        return count;
     }
 }
