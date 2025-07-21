3d8032ac9847a5f33e7e0e559948eaf431a2ccbe
우동균 <96711842+thisis-joe@users.noreply.github.com>
2025-07-21 21:34:34 +0900

    [Silver II] Title: DFS와 BFS, Time: 508 ms, Memory: 38440 KB -BaekjoonHub

---
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -0,0 +1,70 @@
+import java.util.*;
+
+public class Main {
+    static boolean[] visited;
+    static ArrayList<Integer>[] graph;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int N = sc.nextInt(); // 정점 개수
+        int M = sc.nextInt(); // 간선 개수
+        int V = sc.nextInt(); // 시작 정점
+
+        // 그래프 초기화
+        graph = new ArrayList[N + 1];
+        for (int i = 1; i <= N; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        // 간선 입력
+        for (int i = 0; i < M; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            graph[a].add(b);
+            graph[b].add(a);
+        }
+
+        // 정점 번호가 작은 순으로 방문하기 위해 정렬
+        for (int i = 1; i <= N; i++) {
+            Collections.sort(graph[i]);
+        }
+
+        // DFS
+        visited = new boolean[N + 1];
+        dfs(V);
+        System.out.println();
+
+        // BFS
+        visited = new boolean[N + 1];
+        bfs(V);
+        System.out.println();
+    }
+
+    public static void dfs(int node) {
+        visited[node] = true;
+        System.out.print(node + " ");
+        for (int next : graph[node]) {
+            if (!visited[next]) {
+                dfs(next);
+            }
+        }
+    }
+
+    public static void bfs(int start) {
+        Queue<Integer> queue = new LinkedList<>();
+        visited[start] = true;
+        queue.add(start);
+
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
+            System.out.print(node + " ");
+            for (int next : graph[node]) {
+                if (!visited[next]) {
+                    visited[next] = true;
+                    queue.add(next);
+                }
+            }
+        }
+    }
+}
