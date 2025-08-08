10c85185fc168848259d352807c9487b95d75599
우동균 <96711842+thisis-joe@users.noreply.github.com>
2025-08-08 12:23:28 +0900

    [level 2] Title: 디펜스 게임, Time: 107.90 ms, Memory: 137 MB -BaekjoonHub

--- 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
+++ 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
@@ -2,20 +2,24 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int k, int[] enemy) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder()); // 최대힙
-        for (int i = 0; i < enemy.length; i++) {
-            n -= enemy[i];      // 이번 라운드 병사 소모
-            pq.add(enemy[i]);   // 무적권 후보에 넣기
-
-            if (n < 0) {        // 병사 부족 → 무적권 사용
-                if (k > 0) {
-                    n += pq.poll(); // 가장 큰 라운드를 무적권 처리
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        
+        for(int round = 0 ; round < enemy.length ; round++){
+            n -= enemy[round];
+            pq.add(enemy[round]);
+            
+            if(n < 0){ //n이 음수일때 현재까지의 최대 적의 수에서 k를 사용한 것으로 바꿔치기 하자.
+                if(k > 0){ //k가 남아있어야 적용 가능하다. 
+                    n += pq.poll();
                     k--;
-                } else {
-                    return i; // 더 못 막음
+                }
+                else{ //n이 음수이고 k가 없으면 더이상 진행불가하다
+                    return round; //라운드는 1라운드부터
                 }
             }
+            
         }
-        return enemy.length; // 전부 막음
+        
+        return enemy.length;
     }
 }
