888e85a86e978d813e398af009c5fc9aed3b24e4
우동균 <96711842+thisis-joe@users.noreply.github.com>
2025-08-11 14:11:04 +0900

    [level 1] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/README.md
+++ 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/README.md
@@ -1,6 +1,6 @@
 # [level 1] 조건에 부합하는 중고거래 댓글 조회하기 - 164673 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/164673?language=oracle) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/164673) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@ Empty
 
 ### 제출 일자
 
-2024년 12월 19일 16:46:06
+2025년 08월 11일 14:10:59
 
 ### 문제 설명
 
--- 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
+++ 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -1,30 +1,11 @@
--- USED_GOODS_BOARD 
--- BOARD_ID, WRITER_ID, TITLE, CONTENTS, PRICE, CREATED_DATE, STATUS, VIEWS
--- 게시글 ID, 작성자 ID, 게시글 제목, 게시글 내용, 가격, 작성일, 거래상태, 조회수를 의미합니다
+-- 코드를 입력하세요
+SELECT B.TITLE, B.BOARD_ID, R.REPLY_ID, R.WRITER_ID, R.CONTENTS, TO_CHAR(R.CREATED_DATE, 'YYYY-MM-DD') AS CREATED_DATE
+FROM USED_GOODS_BOARD B
+JOIN USED_GOODS_REPLY R
+ON B.BOARD_ID = R.BOARD_ID
+WHERE TO_CHAR(B.CREATED_DATE,'YYYY-MM') = '2022-10'
+ORDER BY R.CREATED_DATE, B.TITLE
 
--- USED_GOODS_REPLY
--- REPLY_ID, BOARD_ID, WRITER_ID, CONTENTS, CREATED_DATE
--- 댓글 ID, 게시글 ID, 작성자 ID, 댓글 내용, 작성일
-
--- 2022년 10월에 작성된 게시글 제목, 게시글 ID, 댓글 ID, 댓글 작성자 ID, 댓글 내용, 댓글 작성일을 조회하는 SQL문
-
-SELECT 
-    A.TITLE, 
-    A.BOARD_ID, 
-    B.REPLY_ID, 
-    B.WRITER_ID, 
-    B.CONTENTS, 
-    TO_CHAR(B.CREATED_DATE,'YYYY-MM-DD') AS CREATED_DATE
-FROM USED_GOODS_BOARD A
-JOIN USED_GOODS_REPLY B
-ON A.BOARD_ID = B.BOARD_ID
-WHERE
-    EXTRACT(YEAR FROM A.CREATED_DATE) = 2022
-    AND
-    EXTRACT(MONTH FROM A.CREATED_DATE) = 10
-ORDER BY
-    B.CREATED_DATE,
-    A.TITLE
-    
-    
--- 어디서부터 조인되었고 안되었는지 모르겠음. JOIN-ON 다음에는 A로 참조하면 조인된 테이블을 참조하는 것인가? SELECT는 그럼 조인 안된?
+-- WRITER_ID도 컬럼이름이 겹치는데 어떻게 구분해야할까?
+-- TO_CHAR를 이용해서 DATE 타입을 변환해야하는 경우는, 
+-- 단순 비교만 
