c32e23b8b411d68b62726bca71a831a188ce5bbb
우동균 <96711842+thisis-joe@users.noreply.github.com>
2025-08-14 12:17:45 +0900

    [level 0] Title: 배열 두배 만들기, Time: 2.90 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.java
+++ 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.java
@@ -1,15 +1,6 @@
 import java.util.*;
-
 class Solution {
     public int[] solution(int[] numbers) {
-        int[] answer = numbers;
-        System.out.println(Arrays.toString(answer)); //자바 배열 출력
-        //for-each문
-        int i=0;
-        for(int num : answer){
-            answer[i]=num*2;
-            i++;
-        }
-        return answer;
+        return Arrays.stream(numbers).map(i->i*2).toArray();
     }
 }
