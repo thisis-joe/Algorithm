8c4b53736914f585713eac17ed9afb81d425ec4a
우동균 <96711842+thisis-joe@users.noreply.github.com>
2025-08-14 12:33:20 +0900

    [level 0] Title: 암호 해독, Time: 11.90 ms, Memory: 87.9 MB -BaekjoonHub

--- 프로그래머스/0/120892. 암호 해독/암호 해독.java
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.java
@@ -1,8 +1,8 @@
 class Solution {
     public String solution(String cipher, int code) {
         String s= "";
-        for(int i = 0 ; i < cipher.length() ; i++){
-            if((i+1)%code==0) s+=cipher.charAt(i);
+        for(int i = code-1 ; i < cipher.length() ; i += code){
+            s += cipher.charAt(i);
         }
         return s;
     }
