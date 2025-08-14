7062f6ed478865505ca21529af74fcd28283b0a0
우동균 <96711842+thisis-joe@users.noreply.github.com>
2025-08-14 17:29:16 +0900

    [level 2] Title: 스킬트리, Time: 10.00 ms, Memory: 87.2 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.java
@@ -2,23 +2,15 @@ import java.util.*;
 
 class Solution {
     public int solution(String skill, String[] skill_trees) {
-        int count = 0;
+        int valid = 0;
+        String regex = "[^" + skill + "]"; // skill에 없는 문자 전부 제거
 
         for (String tree : skill_trees) {
-            StringBuilder filtered = new StringBuilder();
-            
-            for (int i = 0; i < tree.length(); i++) {
-                char c = tree.charAt(i);
-                if (skill.indexOf(c) != -1) {
-                    filtered.append(c);
-                }
-            }
-
-            if (skill.startsWith(filtered.toString())) {
-                count++;
+            String reduced = tree.replaceAll(regex, ""); // 선행 스킬들만 남김
+            if (skill.startsWith(reduced)) {             // 앞부분(prefix)인지 확인
+                valid++;
             }
         }
-
-        return count;
+        return valid;
     }
 }
