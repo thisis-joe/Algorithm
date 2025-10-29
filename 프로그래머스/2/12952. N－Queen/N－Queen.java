328eb7aa85ec922e9e32398a0eeda936db7329ac
우동균 <96711842+thisis-joe@users.noreply.github.com>
2025-10-29 20:41:57 +0900

    [level 2] Title: N-Queen, Time: 220.92 ms, Memory: 80.2 MB -BaekjoonHub

--- 프로그래머스/2/12952. N－Queen/N－Queen.java
+++ 프로그래머스/2/12952. N－Queen/N－Queen.java
@@ -1,23 +1,43 @@
-public class Solution {
-    int n, answer = 0;
-    boolean[] col, d1, d2; // col[c], d1[r-c+n-1], d2[r+c]
+import java.util.*;
 
+class Solution {
+    static int[] map; // p 번째 원소 k : p열 k행 에 퀸 존재를 의미
+    static int caseCnt = 0;
+    
     public int solution(int n) {
-        this.n = n;
-        col = new boolean[n];
-        d1  = new boolean[2*n - 1];
-        d2  = new boolean[2*n - 1];
-        backtrack(0);
-        return answer;
+        map = new int[n];
+        
+        nQueen(0, n);
+        
+        return caseCnt;
     }
-    private void backtrack(int r) {
-        if (r == n) { answer++; return; }
-        for (int c = 0; c < n; c++) {
-            int a = r - c + n - 1, b = r + c;
-            if (col[c] || d1[a] || d2[b]) continue;
-            col[c] = d1[a] = d2[b] = true;
-            backtrack(r + 1);
-            col[c] = d1[a] = d2[b] = false;
+    
+    public static void nQueen(int col,int n){
+        if(col == n){ //마침내 퀸은 마지막 열에서도 가능한 위치가 있다. 케이스 추가
+            caseCnt++;
+            return;
         }
+        
+        for(int row = 0 ; row < n ; row++){ // 행을 하나씩 내려간다.
+            map[col] = row;
+            
+            if(isPossible(col)){      // 다음 열에서 퀸이 위치할 곳이 있으면
+                nQueen(col+1,n); // 그 다음열을 보러 가자.
+            }
+        }
+    }
+    
+    public static boolean isPossible(int col){
+         // preCol는 이전에 이미 배치한 열들을 확인
+        for (int preCol = 0; preCol < col; preCol++) { 
+            if(map[preCol] == map[col]){ //행이 같음 
+                return false;
+            }
+            if(Math.abs(map[preCol]-map[col]) 
+                == Math.abs(preCol-col)){  //대각선이 같음
+                return false;
+            }
+        }
+        return true;
     }
 }
--- 프로그래머스/2/12952. N－Queen/README.md
+++ 프로그래머스/2/12952. N－Queen/README.md
@@ -1,10 +1,10 @@
 # [level 2] N-Queen - 12952 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12952?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12952) 
 
 ### 성능 요약
 
-메모리: 76.4 MB, 시간: 79.62 ms
+메모리: 80.2 MB, 시간: 220.92 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 04일 16:24:49
+2025년 10월 29일 20:41:52
 
 ### 문제 설명
 
