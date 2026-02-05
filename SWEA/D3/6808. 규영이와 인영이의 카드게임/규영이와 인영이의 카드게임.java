549f7ef0cc536f2606964a7a97f782ffdf3c5230
우동균 <96711842+thisis-joe@users.noreply.github.com>
2026-02-05 16:28:58 +0900

    [D3] Title: 규영이와 인영이의 카드게임, Time: 136 ms, Memory: 27,520 KB -BaekjoonHub

--- SWEA/D3/6808. 규영이와 인영이의 카드게임/규영이와 인영이의 카드게임.java
+++ SWEA/D3/6808. 규영이와 인영이의 카드게임/규영이와 인영이의 카드게임.java
@@ -1,88 +1,100 @@
-
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Solution {
-	static final int NINE = 9;
-	static int GYUScore, IENScore, GYUWin, GYULose, IENCardArr[], IENiArr[];
-	static List<Integer> GYUList;
-	static int[] thisCase = new int[9]; 		// 재귀 호출끼리 인영이가 내는 카드 순서를 공유해야 함
-	static boolean[] visited = new boolean[9]; 	// ''
-	
-    public static void main(String[] args) throws NumberFormatException, IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
-          
-        for (int test_case = 1; test_case <= T; test_case++) {
-        	
-        	GYUWin = GYULose = GYUScore = 0; //테스트케이스 초기화
-        	
-        	IENCardArr = new int[9];
-        	GYUList = new ArrayList<>();
-            IENiArr = new int[] {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18};
-            
-            //규영 입력, 인영 카드 확정
-            StringTokenizer stk = new StringTokenizer(br.readLine());
-            while(stk.hasMoreTokens()) {
-            	int card = Integer.parseInt(stk.nextToken());
-            	GYUList.add(card);
-            	IENiArr[card-1] = -1;
-            }
-            IENCardArr = Arrays.stream(IENiArr).filter(a -> a!=-1).toArray();       
-            
-            //경우의 수(순열) 에서 게임 진행
-            permutation(0);
-            
-            //결과 출력
-        	System.out.println("#"+test_case+" "+GYUWin+" "+GYULose);
-        }   
-    }
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     
-    public static void permutation(int cnt) {
-        
-    	//종료
-    	if(cnt == NINE) {
-    		GYUScore = 0;
-    		IENScore = 0;
-    		
-    		//인영 케이스 1개에 대한 게임 (총9판) 진행
-    		for(int i = 0 ; i < NINE ; i++) {
-    			int GYU =  GYUList.get(i);
-    			int IEN = thisCase[i];
-    			if(IEN < GYU) {
-    				GYUScore += GYU + IEN;
-    			}else {
-    				IENScore +=  GYU + IEN;
-    			}
-    		}
-    		
-    		if(GYUScore > IENScore) {
-    			GYUWin++;
-    		}else {
-    			GYULose++;
-    		}
-    		
-    		//게임 초기화
-//    		Arrays.fill(thisCase, 0);
-//    		Arrays.fill(visited, false);
-//    		
-    		return;
-    	}
-    	
-    	//유도
-    	for(int i = 0 ; i < NINE ; i++) {
-    		if(!visited[i]) {
-            	visited[i] = true;
-            	thisCase[cnt] = IENCardArr[i];
-                permutation(cnt+1);
-                visited[i] = false;
+    static final int CARD_COUNT = 9;
+    static final int TOTAL_SCORE = 171;
+    static final int WIN_THRESHOLD = 85;
+    
+    static int[] gyuCards = new int[CARD_COUNT];
+    static int[] ienCards = new int[CARD_COUNT];
+    static int[] factorial = new int[CARD_COUNT + 1]; // 실행 시 계산
+    static int[][] memo = new int[TOTAL_SCORE + 1][1 << CARD_COUNT];
+
+    static void calculateFactorial() {
+        factorial[0] = 1;
+        for (int i = 1; i <= CARD_COUNT; i++) {
+            factorial[i] = factorial[i - 1] * i;
+        }
+    }
+
+    static int solve(int round, int visitedMask, int gyuTotalScore, int ienTotalScore) {
+        if (memo[ienTotalScore][visitedMask] != -1) {
+            return memo[ienTotalScore][visitedMask];
+        }
+
+        if (ienTotalScore > WIN_THRESHOLD) {
+            return memo[ienTotalScore][visitedMask] = factorial[CARD_COUNT - round];
+        }
+        if (gyuTotalScore > WIN_THRESHOLD) {
+            return memo[ienTotalScore][visitedMask] = 0;
+        }
+
+        if (round >= CARD_COUNT) {
+            return ienTotalScore > gyuTotalScore ? 1 : 0;
+        }
+
+        int winCount = 0;
+        for (int i = 0; i < CARD_COUNT; i++) {
+            if ((visitedMask & (1 << i)) == 0) {
+                int currentRoundPoints = gyuCards[round] + ienCards[i];
+                
+                if (gyuCards[round] > ienCards[i]) {
+                    winCount += solve(round + 1, visitedMask | (1 << i), gyuTotalScore + currentRoundPoints, ienTotalScore);
+                } else {
+                    winCount += solve(round + 1, visitedMask | (1 << i), gyuTotalScore, ienTotalScore + currentRoundPoints);
+                }
             }
-    	}
+        }
+
+        return memo[ienTotalScore][visitedMask] = winCount;
+    }
+
+    public static void main(String[] args) throws IOException {
+        calculateFactorial(); // 팩토리얼 미리 계산
+        
+        int totalTestCases = readInt();
+        StringBuilder resultBuilder = new StringBuilder();
+
+        for (int tc = 1; tc <= totalTestCases; tc++) {
+            boolean[] isGyuCard = new boolean[19];
+            for (int i = 0; i < CARD_COUNT; i++) {
+                gyuCards[i] = readInt();
+                isGyuCard[gyuCards[i]] = true;
+            }
+
+            for (int i = 0; i < TOTAL_SCORE + 1; i++) {
+                Arrays.fill(memo[i], -1);
+            }
+
+            int ienIdx = 0;
+            for (int i = 1; i <= 18; i++) {
+                if (!isGyuCard[i]) {
+                    ienCards[ienIdx++] = i;
+                }
+            }
+
+            int ienTotalWins = solve(0, 0, 0, 0);
+            int gyuTotalWins = factorial[CARD_COUNT] - ienTotalWins;
+
+            resultBuilder.append('#').append(tc)
+                         .append(' ').append(gyuTotalWins)
+                         .append(' ').append(ienTotalWins).append('\n');
+        }
+
+        System.out.print(resultBuilder);
+    }
+
+    static int readInt() throws IOException {
+        int n = 0;
+        int c = br.read();
+        while (c <= 32) c = br.read();
+        while ('0' <= c && c <= '9') {
+            n = n * 10 + (c - '0');
+            c = br.read();
+        }
+        return n;
     }
 }
